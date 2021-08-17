689e218d97a129bd49ef997ff1edba22a8dad001
James Jarvis <jjarvis@gravoc.com>
2021-08-17 13:44:08 -0400

    Pull Context Prisma Key From Env With Fallback

--- src/generator/generate-helpers.ts
+++ src/generator/generate-helpers.ts
@@ -19,11 +19,11 @@ export function generateHelpersFile(sourceFile: SourceFile) {
 
   sourceFile.addStatements(/* ts */ `
     export function getPrismaFromContext(context: any) {
-      const prismaClient = context.prisma;
+      const prismaClient = context[${process.env.CONTEXT_PRISMA_KEY} || 'prisma'];
       if (!prismaClient) {
         throw new Error("Unable to find Prisma Client in GraphQL context. Please provide it under the \`context.prisma\` key.");
       }
       return prismaClient;
     }
   `);
-}
+
